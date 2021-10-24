e91594d3d4a9eb10c35c0e9bada65176d1ce1db0
Robin Denton <raenton@gmail.com>
2021-10-24 11:41:52 +0100

    Added basic client request test w/ proxy to server
    - Added axios package
    - Added proxy to package.json
    - Test client request to api

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import axios from 'axios';
+
+async function test() {
+  const resp = await axios.get('/api');
+  console.log(resp);
+}
 
 function App() {
   return (
     <div className="App">
-      ...
+      <button onClick={test}>Test GET '/api'</button>
     </div>
   );
 }
