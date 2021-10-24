5fd35a5de53aad49df4a551a79c072486dc4ea94
Robin Denton <raenton@gmail.com>
2021-10-24 18:13:21 +0100

    Rendered WeblogInspector

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
+import WeblogInspector from './apps/WeblogInspector';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <WeblogInspector />
     </div>
   );
 }
