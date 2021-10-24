afdcc74aaa46d61778e1cbf7cf325988d6fd259a
Robin Denton <raenton@gmail.com>
2021-10-24 16:18:00 +0100

    Refactored weblogService module pattern

--- server/src/weblogService.js
+++ server/src/weblogService.js
@@ -1,100 +1,107 @@
 const { UserInputError } = require('./errors');
 const SORT_ORDERS = ['asc', 'desc'];
 
-function isValidSortOrder(order) {
-  if (order === undefined) {
-    return true;
-  } else {
-    return SORT_ORDERS.indexOf(order) === -1
-      ? false
-      : true;
-  }
-}
-
-function sortPagesByViewCount(pages, order = 'desc') {
-  pages.sort((a, b) => {
-    if (a.views < b.views) {
-      if (order === 'asc') {
-        return -1;
-      } else if (order === 'desc') {
-        return 1;
-      }
-    } else if (a.views > b.views) {
-      if (order === 'asc') {
-        return 1;
-      } else if (order === 'desc') {
-        return -1;
-      }
-    }
-    return 0;
-  });
-};
-
-exports.totalPageViews = function(log, order) {
-  if (!isValidSortOrder(order)) {
-    throw new UserInputError('Invalid sort order detected. Please specify either `asc`, `desc`, or undefined sort order.');
-  }
-
-  const pages = [];
-  const lines = log.split('\n');
-
-  lines.forEach(line => {
-    // avoid empty new lines
-    if (line.length === 0) {
-      return;
-    }
-    const page = line.split(' ')[0];
-
-    const pageIndex = pages.findIndex(p => p.name === page);
-    if (pageIndex === -1) {
-      pages.push({ name: page, views: 1 });
+module.exports = function() {
+  function _isValidSortOrder(order) {
+    if (order === undefined) {
+      return true;
     } else {
-      pages[pageIndex].views++;
+      return SORT_ORDERS.indexOf(order) === -1
+        ? false
+        : true;
     }
-  });
-
-  if (order !== undefined) {
-    sortPagesByViewCount(pages, order);
   }
-
-  return { pages, order };
-};
-
-exports.totalUniquePageViews = function(log, order) {
-  if (!isValidSortOrder(order)) {
-    throw new UserInputError('Invalid sort order detected. Please specify either `asc`, `desc`, or undefined sort order.');
-  }
-
-  const pages = [];
-  const ipTracker = [];
-  const lines = log.split('\n');
-  lines.forEach(line => {
-    // avoid empty new lines
-    if (line.length === 0) {
-      return;
+  
+  function _sortPagesByViewCount(pages, order = 'desc') {
+    pages.sort((a, b) => {
+      if (a.views < b.views) {
+        if (order === 'asc') {
+          return -1;
+        } else if (order === 'desc') {
+          return 1;
+        }
+      } else if (a.views > b.views) {
+        if (order === 'asc') {
+          return 1;
+        } else if (order === 'desc') {
+          return -1;
+        }
+      }
+      return 0;
+    });
+  };
+  
+  function totalPageViews(log, order) {
+    if (!_isValidSortOrder(order)) {
+      throw new UserInputError('Invalid sort order detected. Please specify either `asc`, `desc`, or undefined sort order.');
     }
-    const [page, ip] = line.split(' ');
-
-    const pageIndex = pages.findIndex(p => p.name === page);
-    if (pageIndex === -1) {
-      // page has not been seen yet
-      pages.push({ name: page, views: 1 });
-      ipTracker.push({ name: page, ips: [ip] });
-    } else {
-      // page has been seen
-      const ipTrackedPageIndex = ipTracker.findIndex(p => p.name === page)
-      const ipTrackedPage = ipTracker[ipTrackedPageIndex];
-      if (ipTrackedPage.ips.indexOf(ip) === -1) {
-        // ip has not been seen, unique visit.
+  
+    const pages = [];
+    const lines = log.split('\n');
+  
+    lines.forEach(line => {
+      // avoid empty new lines
+      if (line.length === 0) {
+        return;
+      }
+      const page = line.split(' ')[0];
+  
+      const pageIndex = pages.findIndex(p => p.name === page);
+      if (pageIndex === -1) {
+        pages.push({ name: page, views: 1 });
+      } else {
         pages[pageIndex].views++;
-        ipTrackedPage.ips.push(ip);
       }
+    });
+  
+    if (order !== undefined) {
+      _sortPagesByViewCount(pages, order);
     }
-  });
+  
+    return { pages, order };
+  };
+  
+  function totalUniquePageViews(log, order) {
+    if (!_isValidSortOrder(order)) {
+      throw new UserInputError('Invalid sort order detected. Please specify either `asc`, `desc`, or undefined sort order.');
+    }
+  
+    const pages = [];
+    const ipTracker = [];
+    const lines = log.split('\n');
+    lines.forEach(line => {
+      // avoid empty new lines
+      if (line.length === 0) {
+        return;
+      }
+      const [page, ip] = line.split(' ');
+  
+      const pageIndex = pages.findIndex(p => p.name === page);
+      if (pageIndex === -1) {
+        // page has not been seen yet
+        pages.push({ name: page, views: 1 });
+        ipTracker.push({ name: page, ips: [ip] });
+      } else {
+        // page has been seen
+        const ipTrackedPageIndex = ipTracker.findIndex(p => p.name === page)
+        const ipTrackedPage = ipTracker[ipTrackedPageIndex];
+        if (ipTrackedPage.ips.indexOf(ip) === -1) {
+          // ip has not been seen, unique visit.
+          pages[pageIndex].views++;
+          ipTrackedPage.ips.push(ip);
+        }
+      }
+    });
+  
+    if (order !== undefined) {
+      _sortPagesByViewCount(pages, order);
+    }
+  
+    return { pages, order };
+  };
 
-  if (order !== undefined) {
-    sortPagesByViewCount(pages, order);
-  }
-
-  return { pages, order };
-};
+  return {
+    totalPageViews,
+    totalUniquePageViews
+  };
+}();
