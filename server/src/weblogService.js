30892a05dcbf008f4bffe2bb5d58559e5a7ab879
Robin Denton <raenton@gmail.com>
2021-10-24 14:58:28 +0100

    weblogService.sortPagesByViewCount implemented and tested

--- server/src/weblogService.js
+++ server/src/weblogService.js
@@ -1,5 +1,59 @@
-exports.totalPageViews = function(log, order = 'desc') {
-  
+const SORT_ORDERS = ['asc', 'desc'];
+
+function isValidSortOrder(order) {
+  if (order === undefined) {
+    return true;
+  } else {
+    return SORT_ORDERS.indexOf(order) === -1
+      ? false
+      : true;
+  }
+}
+
+function sortPagesByViewCount(pages, order = 'desc') {
+  pages.sort((a, b) => {
+    if (a.views < b.views) {
+      if (order === 'asc') {
+        return -1;
+      } else if (order === 'desc') {
+        return 1;
+      }
+    } else if (a.views > b.views) {
+      if (order === 'asc') {
+        return 1;
+      } else if (order === 'desc') {
+        return -1;
+      }
+    }
+    return 0;
+  });
+};
+
+exports.totalPageViews = function(log, order) {
+  if (!isValidSortOrder(order)) {
+    throw new Error('Invalid sort order detected. Please specify either `asc`, `desc`, or undefined sort order.');
+  }
+
+  const pages = [];
+  const lines = log.split('\n');
+
+  lines.forEach(line => {
+    const parts = line.split(' ');
+    const page = parts[0];
+
+    const pageIndex = pages.findIndex(p => p.name === page);
+    if (pageIndex === -1) {
+      pages.push({ name: page, views: 1 });
+    } else {
+      pages[pageIndex].views++;
+    }
+  });
+
+  if (order !== undefined) {
+    sortPagesByViewCount(pages, order);
+  }
+
+  return { pages, order };
 };
 
 exports.totalUniquePageViews = function(log, order = 'desc') {
--- server/test/weblogService.test.js
+++ server/test/weblogService.test.js
@@ -1,23 +1,109 @@
 const assert = require('assert');
+const dedent = require('dedent-js');
 const weblogService = require('../src/weblogService');
 
 describe('weblogService', function() {
   describe('#totalPageViews', function() {
-    it('returns a list of unique pages', function() {
-      assert(false);
-    });
-
-    it('returns view counts for pages', function() {
-      assert(false);
+    it('returns an unsorted array of unique pages with view counts', function() {
+      const testData = dedent
+      `/home 316.433.849.805
+      /home 543.910.244.929
+      /contact 061.945.150.735
+      /about 336.284.013.698
+      /about 336.284.013.698`;
+      
+      const result = weblogService.totalPageViews(testData);
+      assert.deepEqual(result.pages, [
+        {
+          name: '/home',
+          views: 2
+        },
+        {
+          name: '/contact',
+          views: 1
+        },
+        {
+          name: '/about',
+          views: 2
+        }
+      ]);
     });
 
     it('returns pages sorted in ascending order by view count', function() {
-      assert(false);
+      const testData = dedent
+      `/home 316.433.849.805
+      /home 543.910.244.929
+      /home 543.910.244.929
+      /about 336.284.013.698
+      /about 336.284.013.698
+      /contact 061.945.150.735`;
+      
+      const result = weblogService.totalPageViews(testData, 'asc');
+      assert.deepEqual(result.pages, [
+        {
+          name: '/contact',
+          views: 1
+        },
+        {
+          name: '/about',
+          views: 2
+        },
+        {
+          name: '/home',
+          views: 3
+        }
+      ]);
     });
 
     it('returns pages sorted in descending order by view count', function() {
-      assert(false);
+      const testData = dedent
+      `/home 316.433.849.805
+      /home 543.910.244.929
+      /home 543.910.244.929
+      /about 336.284.013.698
+      /about 336.284.013.698
+      /contact 061.945.150.735`;
+      
+      const result = weblogService.totalPageViews(testData, 'desc');
+      assert.deepEqual(result.pages, [
+        {
+          name: '/home',
+          views: 3
+        },
+        {
+          name: '/about',
+          views: 2
+        },
+        {
+          name: '/contact',
+          views: 1
+        },
+      ]);
     });
+
+    it('Throws an error when an invalid sort order params are provided', function() {
+      assert.throws(() => {
+        weblogService.totalPageViews('', '1234456789');
+      }, {
+        name: 'Error',
+        message: 'Invalid sort order detected. Please specify either `asc`, `desc`, or undefined sort order.'
+      });
+
+      assert.throws(() => {
+        weblogService.totalPageViews('', 'ASC'); // not 'asc'
+      }, {
+        name: 'Error',
+        message: 'Invalid sort order detected. Please specify either `asc`, `desc`, or undefined sort order.'
+      });
+
+      assert.throws(() => {
+        weblogService.totalPageViews('', 'DESC'); // not 'desc'
+      }, {
+        name: 'Error',
+        message: 'Invalid sort order detected. Please specify either `asc`, `desc`, or undefined sort order.'
+      });
+    });
+
   });
 
   describe('#totalUniquePageViews', function() {
