447c529310945171452db183304451ee9c94e4a3
Robin Denton <raenton@gmail.com>
2021-10-24 13:34:21 +0100

    Added express-fileupload library

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 const api = require('./src/api');
 // Move this into env variable later
 const PORT = 8001;
@@ -8,6 +9,15 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(cors());
 }
+
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: '/tmp/',
+  limits: {
+    fileSize: 1024 * 1024 // 1 MB
+  },
+  abortOnLimit: true
+}));
 
 app.use('/api', api);
 
