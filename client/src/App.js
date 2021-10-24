09bf16cf5ef13d1e0e2fee91a5231ee67f23b4f7
Robin Denton <raenton@gmail.com>
2021-10-24 12:52:04 +0100

    Removed api test from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,9 @@
 import './App.css';
-import client from './api/client';
-
-async function test() {
-  const resp = await client.get('/');
-  console.log(resp);
-}
 
 function App() {
   return (
     <div className="App">
-      <button onClick={test}>Test GET '/api'</button>
+      
     </div>
   );
 }
