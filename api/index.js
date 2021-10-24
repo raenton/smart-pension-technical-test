e5dc104a8adbc12a4e268880cbee4acf7a1af08e
Robin Denton <raenton@gmail.com>
2021-10-24 11:53:38 +0100

    api use cors in development mode
    - Added cors package
    - Added cors as express middleware

--- api/index.js
+++ api/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const api = require('./src/api');
 // Move this into env variable later
 const PORT = 8001;
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors());
+}
 
 app.use('/api', api);
 
