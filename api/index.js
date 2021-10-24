ee721b214841e413a5c390c8266d4410cf100b53
Robin Denton <raenton@gmail.com>
2021-10-24 11:41:00 +0100

    Added basic api route to server

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const api = require('./src/api');
 // Move this into env variable later
 const PORT = 8001;
 const app = express();
+
+app.use('/api', api);
 
 app.listen(PORT, () => console.log(`Listening on: http://localhost:${PORT}`));
