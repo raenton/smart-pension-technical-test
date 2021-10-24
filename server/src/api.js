4092187ff897e0aefbce913045717e2201ea3761
Robin Denton <raenton@gmail.com>
2021-10-24 17:20:48 +0100

    Refactor error handling in server

--- server/src/api.js
+++ server/src/api.js
@@ -1,35 +1,32 @@
 const express = require('express');
 const fs = require('fs');
+const { UserInputError } = require('./errors');
 const weblogService = require('./weblogService');
 
 const router = express.Router();
 
 router.post('/weblog', (req, res, next) => {
-  const { log } = req.files;
-  const { unique, order } = req.query;
+  try {
+    const { log } = req.files;
+    const { unique, order } = req.query;
 
-  if (!req.files) {
-    return res.status(400).send("No files were uploaded.");
-  }
-
-  if (log.mimetype !== 'text/plain') {
-    res.status(400).send("File must be plain text.");
-  }
-
-  fs.readFile(log.tempFilePath, 'utf-8', function (err, data) {
-    if (err) {
-      next(err);
+    if (!req.files) {
+      throw new UserInputError("No files were uploaded.");
     }
 
-    try {
+    fs.readFile(log.tempFilePath, 'utf-8', function (err, data) {
+      if (err) {
+        throw err;
+      }
+
       const result = unique
       ? weblogService.totalUniquePageViews(data, order)
       : weblogService.totalPageViews(data, order);
       res.status(200).json(result);
-    } catch (error) {
-      next(error);
-    }
-  });
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
