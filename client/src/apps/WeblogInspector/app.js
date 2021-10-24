8c7c907d99f6681350d9ef9b483ecbbd0bcac5ee
Robin Denton <raenton@gmail.com>
2021-10-24 18:12:56 +0100

    WeblogInspector app submitWeblog functionality and state

--- client/src/apps/WeblogInspector/app.js
+++ client/src/apps/WeblogInspector/app.js
@@ -1,13 +1,49 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-// import { actions as errorActions } from '../ErrorOverlay/app';
 import api from '../../api';
 
 const APP_NAME = 'weblog_inspector';
 
+const submitWeblog = createAsyncThunk(
+  `${APP_NAME}/submit_weblog`,
+  async ({
+    formData,
+    unique,
+    sortOrder
+  }, {
+    rejectWithValue
+  }) => {
+    try {
+      const response = await api.weblogService.submitWeblog(formData, unique, sortOrder);
+      return response;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+)
+
 const slice = createSlice({
   name: APP_NAME,
   initialState: {
-
+    logReport: {},
+    error: '',
+  },
+  extraReducers: {
+    [submitWeblog.pending]: (state) => {
+      state.logReport = {};
+      state.error = '';
+    },
+    [submitWeblog.fulfilled]: (state, action) => {
+      state.logReport = action.payload;
+    },
+    [submitWeblog.rejected]: (state, action) => {
+      state.error = action.payload.message;
+    }
   }
 });
 
+export const actions = {
+  ...slice.actions,
+  submitWeblog
+};
+
+export default slice.reducer;
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import weblogReducer from '../apps/WeblogInspector/app';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    weblog: weblogReducer
+  },
 });
