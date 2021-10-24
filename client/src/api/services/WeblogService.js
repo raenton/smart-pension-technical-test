20f3014a234301535353a1311d8147876c8f5ff1
Robin Denton <raenton@gmail.com>
2021-10-24 18:14:06 +0100

    Added URL params to WeblogService.submitWeblog for unique and order

--- client/src/api/services/WeblogService.js
+++ client/src/api/services/WeblogService.js
@@ -1,7 +1,9 @@
 function WeblogService(client) {
 
-  async function submitWeblog(formData) {
-    const response = await client.post("/weblog", formData, {
+  async function submitWeblog(formData, unique, order) {
+    // Building the URL like this should be improved upon.
+    const url = `/weblog?unique=${unique}&order=${order}`;
+    const response = await client.post(url, formData, {
       headers: { "Content-Type": "multipart/form-data" },
     });
     return response.data;
