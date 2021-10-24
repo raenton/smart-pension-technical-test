c179c17d4782f53d15dd1b1ca60d808e2bdead19
Robin Denton <raenton@gmail.com>
2021-10-24 11:07:14 +0100

    Fixed missing app declaration in api

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 // Move this into env variable later
 const PORT = 8001;
+const app = express();
 
 app.listen(PORT, () => console.log(`Listening on: http://localhost:${PORT}`));
